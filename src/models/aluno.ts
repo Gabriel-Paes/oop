d565f9166ce7e57e241581cb49a08a20540637c9
Gabriel-Paes <paessouzagabriel@gmail.com>
2024-09-03 19:56:42 -0300

    feat(models): Add method update in class aluno.ts

--- src/models/aluno.ts
+++ src/models/aluno.ts
@@ -9,6 +9,22 @@ export class Aluno {
     this.email = email;
   }
 
+  atualizarAluno(novosDados: {
+    nome?: string;
+    matricula?: number;
+    email?: string;
+  }): void {
+    if (novosDados.nome !== undefined) {
+      this.nome = novosDados.nome;
+    }
+    if (novosDados.matricula !== undefined) {
+      this.matricula = novosDados.matricula;
+    }
+    if (novosDados.email !== undefined) {
+      this.email = novosDados.email;
+    }
+  }
+
   toString(): void {
     console.log(
       `Nome: ${this.nome}\nMatrícula: ${this.matricula}\nE-mail:${this.email}`
