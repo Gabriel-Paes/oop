725aa36c44499a0e910f4bcd181d9888a02307e4
Gabriel-Paes <paessouzagabriel@gmail.com>
2024-12-07 12:42:37 -0300

    fix(models) add typeorm in aluno.ts

--- src/models/aluno.ts
+++ src/models/aluno.ts
@@ -1,33 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity("alunos")
 export class Aluno {
-  nome: string;
-  matricula: number;
-  email: string;
+  @PrimaryGeneratedColumn()
+  matricula!: number;
 
-  constructor(nome: string, matricula: number, email: string) {
-    this.nome = nome;
-    this.matricula = matricula;
-    this.email = email;
-  }
+  @Column({ type: "varchar", length: 255 })
+  nome!: string;
 
-  atualizarAluno(novosDados: {
-    nome?: string;
-    matricula?: number;
-    email?: string;
-  }): void {
+  @Column({ type: "varchar", length: 255, unique: true })
+  email!: string;
+
+  atualizarAluno(novosDados: { nome?: string; email?: string }): void {
     if (novosDados.nome !== undefined) {
       this.nome = novosDados.nome;
-    }
-    if (novosDados.matricula !== undefined) {
-      this.matricula = novosDados.matricula;
     }
     if (novosDados.email !== undefined) {
       this.email = novosDados.email;
     }
   }
 
-  toString(): void {
-    console.log(
-      `Nome: ${this.nome}\nMatrícula: ${this.matricula}\nE-mail: ${this.email}\n`
-    );
+  toString(): string {
+    return `Nome: ${this.nome}\nMatrícula: ${this.matricula}\nE-mail: ${this.email}\n`;
   }
 }
