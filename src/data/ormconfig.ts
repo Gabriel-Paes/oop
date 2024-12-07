af0bf19e16111487f60e187fe262b85f493033cc
Gabriel-Paes <paessouzagabriel@gmail.com>
2024-12-07 12:38:09 -0300

    feat(data) Add typeorm and ormconfig.ts

---
+++ src/data/ormconfig.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Aluno } from "../models/aluno";
+import { Disciplina } from "../models/disciplina";
+import { Turma } from "../models/turma";
+import config from "../config/config";
+
+export const AppDataSource = new DataSource({
+  type: "mysql",
+  host: config.db.host,
+  port: config.db.port,
+  username: config.db.username,
+  password: config.db.password,
+  database: config.db.database,
+  entities: [Aluno, Disciplina, Turma],
+  synchronize: true,
+});
